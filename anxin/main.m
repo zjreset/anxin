6cd2f9def57e9ee3a207688448dc0371c884cf0e
zjreset <admin.zj@163.com>
2014-03-05 16:10:36 +0800

    Initial Commit

---
+++ anxin/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  anxin
+//
+//  Created by runes on 14-3-5.
+//  Copyright (c) 2014年 heigesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
